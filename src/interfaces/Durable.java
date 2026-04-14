fb7f8204a2c1520eb29db6517672a73e492a3d0d
AriffMedina <ariffmedina.contact@gmail.com>
2026-04-13 19:34:40 -0600

    Ajuste en sistema de directorios (excepciones e interfaces)

--- src/interfaces/Durable.java
+++ src/interfaces/Durable.java
@@ -2,6 +2,8 @@ package interfaces;
 
 public interface Durable {
     void reducirDurabilidad(int cantidad);
+
     void reparar(int cantidad);
+
     boolean estaRota();
 }
--- src/interfaces/Vida.java
+++ src/interfaces/Vida.java
@@ -2,5 +2,6 @@ package interfaces;
 
 public interface Vida {
     void recibirDanio(int cantidad);
+
     boolean estaVivo();
 }
